2e3adde3d5fc40766a16a9a76ba0cc95a5faff3f
Zela2520 <sugarbeatzela@gmail.com>
2022-11-03 02:27:46 +0300

    msd sort done

--- module1/msd.cpp
+++ module1/msd.cpp
@@ -13,126 +13,151 @@
 // ab
 
 // Замечания:
-// нужно по сути сделать строку, в которую будем записывать все символы. // std::string s(size, '\0');
-// возможно лучше все-таки создать массив массивов, чтобы 
-// хочу менять элементы по указателю и менять указатели
+// counterArray - массив, индексы которого являются все возможными значениями словаря. В ячейках содержится количество раз, которое встретился тот или иной элемент.
+
+// for (size_t i = 0; i < size; ++i) ++counter[arr[i]]; // говорит сколько раз встретился элемент в словаре.
+
+// for (size_t i = 1; i < avaliableValues; ++i) counter[i] += counter[i - 1]; // формируем группы элементов путем суммирования элементов, которые встречались  в словаре. Таким образом понимаем на какой позиции находится та или иная группа одинаковых элементов. Чтобы извлекать элементы группы достаточно сделать следующее обращение: --counter[arr[i]];
+
+
+// Как трактовать такую запись ? res[--counter[arr[i]]] = arr[i];
+// Ответ:  извлекаем i-ый элемент на последнюю позицию в его группе. (следующее извлечение на предпоследную позицию и т.д.)
 
 #include <iostream>
 #include <vector>
 #include <cstring>
 #include <string.h>
 #include <string>
+#include <cassert>
 
 
 #define MAX_ROWS_NUMBER 100000
 #define ALPHABET_SIZE 256
 
+struct CounterHelper {
+    size_t stringNumber;
+    char curCharacter;
+};
 
-// заполнить строки нулями до максимальной строки в массиве
-// написать цикл, который будет формировать массив из одинаковых разрядов
-// написать функцию, которая будет сортировать
-// отправлять массивы нв сортировку потом сливать 
+// Что мы должны передавать в функцию counterSort ?
+// 1 аргумент - начало группы
+// 2 аргумент - конец группы
+// 3 аргумент - индекс, по которому производится сортировка в группе
 
-// после выполнения этой функции получим массив строк, в котором будут отсортированы буквы по индексу sortIndex
-// void counterSort(std::string *arr, size_t size, size_t sortIndex, size_t maxStringSizeInArray) {
-//     int* counterArray = new int[ALPHABET_SIZE];
-//     for (size_t i = 0; i < ALPHABET_SIZE; ++i) counterArray[i] = 0;
-//     for (size_t i = 0; i < size; ++i) ++counterArray[static_cast<int>(arr[i][sortIndex])]; // массив для определенного разряда - строка размером равным количеству строк
 
-//     // for (size_t i = 0; i < size; ++i) std::cout << arr[i][sortIndex] << " " << static_cast<int>(arr[i][sortIndex]) << std::endl; // отладка
+// counterSort - работает: сортирует строки по символу
+// void counterSort(std::string *arr , size_t size, int* counterArray, size_t maxStringSizeInArray) 
+// void counterSort(std::string *arr , size_t size, size_t maxStringSizeInArray)
 
-//     for (size_t i = 1; i < ALPHABET_SIZE; ++i) {
-//         counterArray[i] += counterArray[i - 1];
-//     }
-
-//     std::string result[size];
-    
-//     for (int i = size - 1; i >= 0; --i) {
-//         result[--counterArray[arr[i][sortIndex]]] = arr[i];
-//     }
-
-//     for (size_t i = 0; i < size; ++i) {
-//         arr[i] = result[i];
-//     }
-
-//     delete[] counterArray;
-//     counterArray =  nullptr;
-// }
-
-// void MSDSort(std::string* arr, size_t size, size_t alfavitSize, size_t maxStringSizeInArray) {
-//     for (size_t i = 0; i < maxStringSizeInArray; ++i) {
-//         counterSort(arr, size, i, maxStringSizeInArray);
-//     }
-// }
-
-// void run(std::istream& input, std::ostream& output) {
-//     std::string stringArray[MAX_ROWS_NUMBER];
-//     size_t arraySize = 0;
-//     size_t maxStringSizeInArray = 0; 
-
-//     std::string buffer = "";
-
-//     while (getline(input, buffer) && buffer != "\0") {
-//         stringArray[arraySize] = buffer;
-
-//         if (stringArray[arraySize].size() > maxStringSizeInArray) {
-//             maxStringSizeInArray = stringArray[arraySize].size();
-//         }
-//         ++arraySize;
-//     }
-
-//     MSDSort(stringArray, arraySize, ALPHABET_SIZE, maxStringSizeInArray);
-
-//     for (size_t i = 0; i < arraySize; ++i) {
-//         output << stringArray[i] << std::endl;
-//     }
-// }
-
-void counterSort(std::string *arr, size_t size, size_t sort_index, size_t maxStringSizeInArray) {
-    // if (size <= 1) return;
-    int* counterArray = new int[ALPHABET_SIZE];
+void counterSort(std::string *arr , size_t size, int* counterArray, size_t sortIndex) {
+    // int* counterArray = new int[ALPHABET_SIZE];
     for (size_t i = 0; i < ALPHABET_SIZE; ++i) counterArray[i] = 0;
-    for (size_t i = 0; i < size; ++i) ++counterArray[static_cast<int>(arr[i][sort_index])]; // массив для определенного разряда - строка размером равным количеству строк
+    for (size_t i = 0; i < size; ++i) ++counterArray[arr[i][sortIndex]]; // заполняем словарь первыми буквами группы
 
     for (size_t i = 1; i < ALPHABET_SIZE; ++i) {
         counterArray[i] += counterArray[i - 1];
     }
 
-    // for (size_t i = 0; i < size; ++i) {
-    //     counterSort(arr + counterArray[i], counterArray[i + 1] - counterArray[i], sort_index + 1, maxStringSizeInArray - 1);
-    // }
-    
-    
     std::string result[size];
     
     for (int i = size - 1; i >= 0; --i) {
-        result[--counterArray[arr[i][sort_index]]] = arr[i];
+        result[--counterArray[arr[i][sortIndex]]] = arr[i];
     }
 
     for (size_t i = 0; i < size; ++i) {
         arr[i] = result[i];
     }
+}
 
+void MSDSort(std::string* arr, size_t size, size_t sortIndex, size_t maxStringSizeInArray) {
+    if (size <= 1) return;
+    // отладка
+    // std::cout << "SortIndex " << sortIndex << " Array size is: " <<  size << std::endl;
+    // for (size_t i = 0; i < size; ++i) {
+    //     std::cout << arr[i] << ' ';
+    // }
+    // std::cout << std::endl;
+    int* counterArray = new int[ALPHABET_SIZE];
+    counterSort(arr, size, counterArray, sortIndex++); // после этой строки получаем отсортированный по первому разряду массив строк
+    if (maxStringSizeInArray > sortIndex) {
+        for (int i = 1; i < ALPHABET_SIZE; ++i) {
+            // std::cout << *(arr + counterArray[i - 1]) << "\t" << counterArray[i] - counterArray[i - 1] << std::endl;
+            MSDSort(arr + counterArray[i - 1], counterArray[i] - counterArray[i - 1], sortIndex, maxStringSizeInArray);
+        }
+    }
     delete[] counterArray;
-    counterArray =  nullptr;
 }
 
 void test() {
     std::cout << "***********TEST CASES************" << std::endl;
-    std::cout << "*****CounterSort case" << std::endl;
+    std::cout << "*****MSDSort case" << std::endl;
     {
-        std::string s[4] = {"cc", "cb", "ca", "ba"};
-        counterSort(s, 4, 0, 2);
-        for (size_t i = 0; i < 4; ++i) {
-            std::cout << s[i] << std::endl;
+        std::cout << "Case 1: " << std::endl;
+        {   
+            constexpr auto arraySize = 4;
+            size_t maxStringSize = 3;
+            std::string s[arraySize] = {"bb", "a", "daa", "ba"};
+
+            MSDSort(s, arraySize, 0, maxStringSize);
+
+            for (size_t i = 0; i < arraySize; ++i) {
+                std::cout << s[i] << std::endl;
+            }
         }
+        std::cout << "OK" << std::endl << std::endl;
+        std::cout << "Case 2: " << std::endl;
+        {   
+            constexpr auto arraySize = 2;
+            size_t maxStringSize = 2;
+            std::string s[arraySize] = {"bb", "bb"};
+            MSDSort(s, arraySize, 0, maxStringSize);
+            for (size_t i = 0; i < arraySize; ++i) {
+                std::cout << s[i] << std::endl;
+            }
+        }
+        std::cout << "OK" << std::endl << std::endl;
+        std::cout << "Case 3: ";
+        {   
+            constexpr auto arraySize = 3;
+            size_t maxStringSize = 2;
+            std::string s[arraySize] = {"bb", "", "a"};
+            MSDSort(s, arraySize, 0, maxStringSize);
+            for (size_t i = 0; i < arraySize; ++i) {
+                std::cout << s[i] << std::endl;
+            }
+        }
+        std::cout << "OK" << std::endl;
     }
-    std::cout << "OK\n";
+    
+    std::cout << "*****OK\n";
     std::cout << "***********All tests passed***********\n";
 }
 
-int main() {assdcds
-    test();
-    // run(std::cin, std::cout);
+void run(std::istream& input, std::ostream& output) {
+    std::string stringArray[MAX_ROWS_NUMBER];
+    size_t arraySize = 0;
+    size_t maxStringSizeInArray = 0; 
+
+    std::string buffer = "";
+
+    while (getline(input, buffer) && buffer != "\0") {
+        stringArray[arraySize] = buffer;
+
+        if (stringArray[arraySize].size() > maxStringSizeInArray) {
+            maxStringSizeInArray = stringArray[arraySize].size();
+        }
+        ++arraySize;
+    }
+
+    MSDSort(stringArray, arraySize, 0, maxStringSizeInArray);
+
+    for (size_t i = 0; i < arraySize; ++i) {
+        output << stringArray[i] << std::endl;
+    }
+}
+
+int main() {
+    // test();
+    run(std::cin, std::cout);
     return 0;
 }
