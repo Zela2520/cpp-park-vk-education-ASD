078ba6267ebd76d3d658f810f135bdecc9601087
Zela2520 <sugarbeatzela@gmail.com>
2022-11-03 17:21:56 +0300

    kStatistic have some problem

--- module1/kStatistic.cpp
+++ module1/kStatistic.cpp
@@ -1,77 +1,125 @@
-// Условие:
-// Реализуйте стратегию выбора опорного элемента “медиана трёх”.
-// Функцию Partition реализуйте методом прохода двумя итераторами от начала массива к концу.
-
-
-
-
-
-// Логика:
-// Найти знчения элемента, который будет стоять в массиве на k-ом индексе, если массив будет отсортированн
-// Отсортировать массив и взять индекс элемента, которого мы хотим найти
-
-// Двигаем итератор i -> пока соблюдается условие, потом двигаем итератор j <- пока выполняется условие 
-// В итоге в момент не соблюдения i и j итераторы будут указывать на элементы, которые должны будут лежать в разных частях массива
-// Меняем элементы местами элементы, на которые они указывают
-// В итоге i-ый элемент будет указывать на элемент, который больше pivat - меняем pivat с элементом, на который указывает i-ый итератор
- 
-
 #include <iostream>
-#include <vector>
+#include <cassert>
 
 
-int partition(int* arr, int l, int r) {
-    int pivotIndex = l + (r - l) / 2;
-    int pivotVal = arr[pivotIndex];
+bool min(const int& left, const int& right) {
+    return left < right;
+}
+
+int getMedianIndex(int* arr, int l, int middle, int r) {
+    if (arr[l] > arr[middle]) {
+        if (arr[r] > arr[l]) {
+            return l;
+        }
+        return (arr[middle] > arr[r]) ? middle : r;
+    }
+    if (arr[r] > arr[middle]) {
+        return middle;
+    }
+    return (arr[l] > arr[r]) ? l : r;
+}
+
+int partition(int* arr, int l, int r, bool(*comparator)(const int& l, const int& r)) {
+    int pivotIndex = getMedianIndex(arr, l, (l + r) / 2, r - 1);
+    // int pivotIndex = l + (r - l) / 2;
+    int pivotValue = arr[pivotIndex];
 
     std::swap(arr[pivotIndex], arr[r - 1]);
-
+    
     int i = 0;
-    int j = r - 1;
+    int j = 0;
 
-    while (i < j) {
-        for (; arr[i] < pivotVal; ++i);
-        while(!(arr[i] < pivotVal)) --j;
+    while (j < r - 1) {
+        while (comparator(arr[i],pivotValue)) {
+            ++i;
+            ++j;
+        }
+        for (; !comparator(arr[j], pivotValue); ++j);
+        if (j < r - 1) {
+            std::swap(arr[i], arr[j]);
+            ++i;
+            ++j;
+        }
     }
-     
-    if (i < j) {
-        std::swap(arr[i], arr[j]);
-        i++; 
-        j--;
-    }
-
     std::swap(arr[i], arr[r - 1]);
     return i;
 }
 
-// после выполнения этой функции, в массиве arr на позиции k будет стоять элемент такой если бы массив был остортирован  
-void ktn_element(int* arr, size_t size, size_t k) {
+void ktn_element(int* arr, int size, int k, bool(*comparator)(const int& l, const int& r)) {
     int l = 0;
     int r = size;
 
-    while(l < r) {
-        int m = partition(arr, l, r);
-        if (m == k) {
-            return;
-        } else if (m < k) {
-            l = m + 1; 
+    while (comparator(l, r)) {
+        int m = partition(arr, l, r, comparator);
+        if (m == k) return;
+        else if (comparator(m, k)) {
+            l = m + 1;
         } else {
             r = m;
         }
     }
+
+}
+
+void printResult(int* arr, int size, bool(*comparator)(const int& l, const int& r)) {
+    ktn_element(arr, size, size * 0.1, min);
+    std::cout << arr[static_cast<int>(size * 0.1)] << std::endl;
+    ktn_element(arr, size, size * 0.5, min);
+    std::cout << arr[static_cast<int>(size * 0.5)] << std::endl;
+    ktn_element(arr, size, size * 0.9, min);
+    std::cout << arr[static_cast<int>(size * 0.9)] << std::endl;
+}
+
+
+void test() {
+    std::cout << "***********TEST CASES************" << std::endl;
+    std::cout << "***********ktnElement function test******** " << std::endl;
+    {   std::cout << "Case 1: ";
+        {
+            int* arr = new int[8]{2, 6, 1, 4, 3, 8, 5, 7};
+            ktn_element(arr, 8, 0, min);
+            assert(arr[0] == 1);
+            delete [] arr;
+            arr = nullptr;
+        }
+        std::cout << "OK" << std::endl;
+        std::cout << "Case 2: ";
+        {
+            int* arr = new int[8]{2, 6, 1, 4, 3, 8, 5, 7};
+            ktn_element(arr, 8, 7, min);
+            assert(arr[7] == 8);
+            delete[] arr;
+            arr = nullptr;
+        }
+        std::cout << "OK" << std::endl;
+        std::cout << "Case 3: ";
+        {
+            int* arr = new int[8]{2, 6, 1, 4, 3, 8, 5, 7};
+            ktn_element(arr, 8, 5, min);
+            assert(arr[5] == 6);
+            delete[] arr;
+            arr = nullptr;
+        }
+        std::cout << "OK" << std::endl;
+    }
+    std::cout << "**********OK**********" << std::endl;
+    std::cout << std::endl;
+    std::cout << "***********All tests passed***********\n";
 }
 
 void run(std::istream& input, std::ostream& output) {
-    std::vector<int> arr = {5, 4, 3, 2, 1, 9, 5, 8, 6, 7};
+    size_t arraySize = -1;
+    input >> arraySize;
+    
+    int* array = new int[arraySize]{0};
+    for (size_t i = 0; i < arraySize; ++i) {
+        input >> array[i];
+    }
 
-    size_t k = 3;
-
-    ktn_element(&arr[0], arr.size(), k);
-
-    output << "ktn element: " << arr[k] << std::endl;
+    printResult(array, arraySize, min);
 }
 
 int main() {
-    run(std::cin, std::cout);
-    return 0;
+    test();
+    // run(std::cin, std::cout);
 }
